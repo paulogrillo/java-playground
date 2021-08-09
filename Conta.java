47d0af2a93938a677a35e94a76c1e56cb0a57afd
Paulo <paulodigitalart@gmail.com>
2021-08-09 17:22:22 -0300

    meu primeiro código java

---
+++ Conta.java
@@ -0,0 +1,21 @@
+public class Conta {
+
+    //Atributos
+    private long numeroConta;
+    private double saldo = 0;
+
+    //Construtor
+    public Conta(long numeroConta){
+        this.numeroConta = numeroConta;
+    }
+
+    //Métodos
+    public boolean sacar(double valor){
+        if(this.saldo>=valor){
+            this.saldo = this.saldo - valor;
+            return true;
+        }else {
+            return  false;
+        }
+    }
+}
